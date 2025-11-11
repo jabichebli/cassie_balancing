c5667b4820318605066cbf2ff28e88584f81d9c3
Jason Abi Chebli <jabi0003@student.monash.edu>
2025-11-11 04:29:44 -0800

    Played with the base controller and external forces and tried to implement my own controller but didnt work better than the original basic controller so I deleted it

--- cassie/studentController.m
+++ cassie/studentController.m
@@ -14,8 +14,16 @@ dq = s(model.n+1 : 2*model.n);
 % tau = zeros(10,1);
 
 %% [Control #2] High Gain Joint PD control on all actuated joints
-kp = 500 ;
-kd = 100 ;
+kp = 1000 ;
+kd = 250 ;
 x0 = getInitialState(model);
 q0 = x0(1:model.n) ;
 tau = -kp*(q(model.actuated_idx)-q0(model.actuated_idx)) - kd*dq(model.actuated_idx) ;
+
+%% [Control #3] Position control based balancing
+
+%% [Control #4] Contact-Force based balancing
+
+%% [Control #5] Momentum-based balancing
+
+%% [Control #6] Push recovery and stepping based balancing
